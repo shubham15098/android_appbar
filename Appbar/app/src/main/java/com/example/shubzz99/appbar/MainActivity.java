f540b384070e703be913dd7b1c2ff8940ff46bea
shubham15098 <shubham15098@iiitd.ac.in>
2017-06-01 03:48:41 +0530

    adding folder

---
+++ Appbar/app/src/main/java/com/example/shubzz99/appbar/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.shubzz99.appbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.v("MainActivity","setting1");
+        }
+        else if(id == R.id.action_settings2)
+        {
+            Log.v("MainActivity","setting2");
+        }
+        else if(id == R.id.action_search)
+        {
+            Log.v("MainActivity","search");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
